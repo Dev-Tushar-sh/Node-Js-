ad335854bce594a56d6a64c3f7861949ea0e3a2b
Dev-Tushar-sh <93375756+Dev-Tushar-sh@users.noreply.github.com>
2024-02-06 15:08:14 +0530

    Create first.js

---
+++ Project1/first.js
@@ -0,0 +1,15 @@
+
+const math = require("./second")
+// const {add, sub} = requir("./second")
+// direct call the functions add(a,b)
+
+
+
+console.log("Hello world");
+console.log("This is my first file in node js");
+
+console.log(math.add(5,5));
+console.log(math.sub(5,5));
+console.log();
+// console.log(add(5,5));
+// console.log(package.json);
